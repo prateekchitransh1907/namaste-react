5c1f445b3ad6ca5b77eb4d906cb169f7e825433d
“Prateekchitransh1907” <prateekchitransh@gmail.com>
2024-10-20 16:38:06 +0530

    chapter 2 completed

--- App.js
+++ App.js
@@ -1,26 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "React padh rele apun neele mein!"
 );
 
-/**
- * Lets try to render a nested element
- * <div id="parent">
- *    <div id="child">
- *       <h1>Main to tag hu bro!</h1>
- *     </div>
- * </div>
- *
- *
- */
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement(
     "div",
     { id: "child" },
-    React.createElement("h1", {}, "main to bas ek tag hu bro!")
+    React.createElement("h1", {}, "😀 main to bas ek tag hu bro! 🏷")
   )
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
